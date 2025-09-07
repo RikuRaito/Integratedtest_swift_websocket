e0946bf17c3f831cf9dc185a8e09d55a3a3cd8f9
RikuRaito <sairikuneont@gmail.com>
2025-09-07 23:37:46 +1000

    Initial commit for swift

---
+++ Websocket_test/Websocket_test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Websocket_test
+//
+//  Created by 斉藤吏功 on 2025/09/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Websocket_test/Websocket_test/Websocket_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Websocket_testApp.swift
+//  Websocket_test
+//
+//  Created by 斉藤吏功 on 2025/09/07.
+//
+
+import SwiftUI
+
+@main
+struct Websocket_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
